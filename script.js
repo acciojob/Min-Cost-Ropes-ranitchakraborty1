16b0f86b08204d852e6e07c4c97bb92d042297d6
ranitchakraborty1 <162864925+ranitchakraborty1@users.noreply.github.com>
2024-08-30 10:28:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function mincost(arr)
 { 
 //write your code here
+	totalCost= 0;
+	while(arr.length>1){
+		arr.sort();
+		let firstSmallest = arr.shift();
+		let secondSmallest = arr.shift();
+		 let smallestCost = firstSmallest + secondSmallest;
+		arr.push(smallestCost);
+		totalCost += smallestCost;
+	}
 // return the min cost
-  
+  return (totalCost);
 }
 
 module.exports=mincost;
